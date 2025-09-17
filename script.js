c252b2e363407000fae44f0f1521f0ca232e22cf
snehalb2005 <01fe23bcs174@kletech.ac.in>
2025-09-17 22:45:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // Placeholder for future JS (like dynamic content)
 console.log("DisasterReady JS loaded");
+function loginStudent(e) {
+  e.preventDefault();
+  const email = document.getElementById('email').value;
+  localStorage.setItem('studentEmail', email); // remember email
+  window.location.href = 'student-dashboard.html';
+}
 
